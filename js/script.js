024363e032bf95bdd8459cf113b3a71b7702856d
naeemali321123 <naeemali321123@gmail.com>
2021-11-22 03:44:04 +0500

    complete assignment04

--- js/script.js
+++ js/script.js
@@ -2,42 +2,20 @@
 // var num2 = parseInt(prompt("Enter 2nd Numer"));
 // var num3 = parseInt(prompt("Enter 3rd Numer"));
 
-// var maxNumber1 = num1 > num2 && num1 > num3;
+// var maxNumber1 = num1 > num2 && num2 > num3;
 // var maxNumber2 = num2 > num1 && num2 > num3;
 // var maxNumber3 = num3 > num1 && num3 > num2;
 
 
-// if(num1 === num2 && num1 === num3){
+// if (num1 === num2 && num1 === num3) {
 //     console.log("Numbers are equal your 1st Number ", num1, "2nd-Number ", num2, "3rd-Number ", num3);
-// }else
-//     if(maxNumber1){
-//         console.log("Number One is greader than Number Two and Three yourMaxNumber is", num1);
-//     }else{ 
-//         if(maxNumber2){
+// } else
+// if (maxNumber1) {
+//     console.log("Number One is greader than Number Two and Three yourMaxNumber is", num1);
+// } else {
+//     if (maxNumber2) {
 //         console.log("Number Two is greader than Number One & Three yourMaxNumber is", num2);
-//     }else
-//         if(maxNumber3)
+//     } else
+//     if (maxNumber3)
 //         console.log("Number Three is greader than Number One & Two yourMaxNumber is", num3);
-//     }
-
-var num1 = parseInt(prompt("Enter 1st Numer"));
-var num2 = parseInt(prompt("Enter 2nd Numer"));
-var num3 = parseInt(prompt("Enter 3rd Numer"));
-
-var maxNumber1 = num1 > num2 && num2 > num3;
-var maxNumber2 = num2 > num1 && num2 > num3;
-var maxNumber3 = num3 > num1 && num3 > num2;
-
-
-if(num1 === num2 && num1 === num3){
-    console.log("Numbers are equal your 1st Number ", num1, "2nd-Number ", num2, "3rd-Number ", num3);
-}else
-    if(maxNumber1){
-        console.log("Number One is greader than Number Two and Three yourMaxNumber is", num1);
-    }else{ 
-        if(maxNumber2){
-        console.log("Number Two is greader than Number One & Three yourMaxNumber is", num2);
-    }else
-        if(maxNumber3)
-        console.log("Number Three is greader than Number One & Two yourMaxNumber is", num3);
-    }
+// }
